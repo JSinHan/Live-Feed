b1edf3e5ccde42a1a873c4e85b1bd26d4db04f28
JSinHan <1241592904@qq.com>
2016-03-02 10:46:57 +0800

    Initial Commit

---
+++ Live Feed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Live Feed
+//
+//  Created by JSin_Han on 16/3/2.
+//  Copyright © 2016年 JSin_Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Live Feed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Live Feed
+//
+//  Created by JSin_Han on 16/3/2.
+//  Copyright © 2016年 JSin_Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
